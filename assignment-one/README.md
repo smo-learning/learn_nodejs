a17169314929c445479fe1fd799056b95b017845
Allquantor <nilssmol@gmail.com>
2019-07-06 15:17:28 +0200

    Task 3 + Task 4

--- assignment-one/README.md
+++ assignment-one/README.md
@@ -3,8 +3,8 @@ Spin up a Node.js-driven Server
 # Task 2
 Handle two Routes: "/" and "/users"
 * Return some greeting text on "/"
-* eturn a list of dummy users
+* Return a list of dummy users
 # Task 3
 Add a form with a "username" input to the "/" page and submit a POST request to "/create-user" upon a button click
-#Task 4
+# Task 4
 Add the "/create-user" route and parse the incoming data and log it to the console
--- assignment-one/app.js
+++ assignment-one/app.js
@@ -2,16 +2,33 @@ const http = require("http");
 
 http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+  let responseStatus = 200;
+  res.setHeader('Content-Type', 'text/html')
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    })
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+    responseStatus = 302;
+    res.setHeader('Location', '/');
+  }
   if (url === "/") {
     res.write('Hello World!');
+    res.write("<form action='/create-user' method='POST'>");
+    res.write("<input type='text' name='message' />");
+    res.write("<button type='submit'>Click</button>");
+    res.write("</form>");
   }
   if (url === "/users") {
     let users = [1, 2, 3].map(i => "<li>User " + i + "</li>");
-    res.write("<ul>"+users+"</ul>");
+    res.write("<ul>" + users + "</ul>");
   }
-
+  res.statusCode = responseStatus;
   res.end();
 }).listen(3000);
